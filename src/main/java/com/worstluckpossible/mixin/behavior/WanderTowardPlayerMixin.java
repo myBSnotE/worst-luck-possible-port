8f2f2532d5c7c10923d35978cb2441a7ce6141a6
HyBri:D <elenabaanova313@gmail.com>
2026-09-22 01:58:30 +0400

    limit player pursuit goal to hostile mobs

--- src/main/java/com/worstluckpossible/mixin/behavior/WanderTowardPlayerMixin.java
+++ src/main/java/com/worstluckpossible/mixin/behavior/WanderTowardPlayerMixin.java
@@ -3,6 +3,7 @@ package com.worstluckpossible.mixin.behavior;
 import com.worstluckpossible.ai.ApproachNearestPlayerGoal;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.world.World;
@@ -12,14 +13,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/** Adds the original priority-4 approach-player goal to path-aware mobs. */
+/** Adds the original priority-4 approach-player goal to hostile path-aware mobs. */
 @Mixin(MobEntity.class)
 public class WanderTowardPlayerMixin {
 	@Shadow protected GoalSelector goalSelector;
 
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void worstluck$addApproachGoal(EntityType<? extends MobEntity> type, World world, CallbackInfo ci) {
-		if ((Object) this instanceof PathAwareEntity pathAware) {
+		if ((Object) this instanceof HostileEntity hostile && hostile instanceof PathAwareEntity pathAware) {
 			goalSelector.add(4, new ApproachNearestPlayerGoal(pathAware));
 		}
 	}
